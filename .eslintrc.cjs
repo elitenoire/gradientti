1ad4c484ec431061612c7d67a4d8716d6ec7521e
Eva Raymond <evathecoderr@outlook.com>
2022-12-05 17:22:19 +0100

    🚨 disable lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,11 @@ module.exports = {
   plugins: ['react', 'prettier', 'tailwindcss'],
   rules: {
     'no-unused-vars': 1,
+    'no-underscore-dangle': 0,
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': 0,
+    'react/require-default-props': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
   },
 }
