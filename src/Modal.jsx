c06ade8f44ba107e270c6614c018abb9dec199be
Eva Raymond <evathecoderr@outlook.com>
2022-12-05 17:21:30 +0100

    ✨ modal from headlessui

---
+++ src/Modal.jsx
@@ -0,0 +1,52 @@
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+function Modal({ open, onClose, children }) {
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
+
+function ModalTitle({ as = 'h3', className = '', children }) {
+  return (
+    <Dialog.Title as={as} className={`text-lg font-bold leading-6 text-gray-900 ${className}`}>
+      {children}
+    </Dialog.Title>
+  )
+}
+
+Modal.Title = ModalTitle
+
+export default Modal
