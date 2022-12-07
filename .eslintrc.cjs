c5e87140df929abf06e55637482d493e47dfa8b8
Eva Raymond <evathecoderr@outlook.com>
2022-12-07 20:10:19 +0100

    🚨 fix lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,10 +26,12 @@ module.exports = {
     'react/require-default-props': 0,
     'react/prop-types': 0,
     'react/jsx-props-no-spreading': 0,
+    'jsx-a11y/label-has-associated-control': [2, { assert: 'either' }],
   },
   settings: {
     tailwindcss: {
-      whitelist: ['animate\\-fadeIn', 'line\\-numbers', 'language\\-css'],
+      config: './tailwind.config.cjs',
+      whitelist: ['line\\-numbers', 'language\\-css'],
     },
   },
 }
